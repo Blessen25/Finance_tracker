f550b7956c0c96c1cdc09e87c469210554cc57c9
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-29 22:27:44 +1000

    Adding css styles

--- frontend/finance/src/App.tsx
+++ frontend/finance/src/App.tsx
@@ -4,8 +4,11 @@ const App = () => {
 
   return(
     <>
-    <div className="w-100">
-      <div className="container border_black"></div>
+    <div className="w-100 cstm_flex flex_j-center flex_a-center">
+      <div className="container_cstm border_black">
+
+        
+      </div>
     </div>
     </>
   )
