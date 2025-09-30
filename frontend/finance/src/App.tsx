1f98204d71e5cbf24aa2ad0720e3fddf6b6416b4
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-30 15:26:51 +1000

    Added Header to screens

--- frontend/finance/src/App.tsx
+++ frontend/finance/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import MainContainer from "./component/main_container";
+import { Header } from "./screens/header/header";
 
 const App = () => {
 
   return(
     <>
-      
+      <Header />
     </>
   )
 }
--- frontend/finance/src/screens/header/header.tsx
+++ frontend/finance/src/screens/header/header.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import './home.css';
+import './header.css';
 
-export const Home = () => {
+export const Header = () => {
 
     return(
 
         <>
         
-        
         </>
     )
 }
+
