76825f9005df1a634dfc1b3cc0d24fc3b60c6905
Blessen25 <blessenvinoymathew25@gmail.com>
2025-10-06 12:52:41 +1100

    Commited Changes

--- frontend/finance/src/screens/login/login.tsx
+++ frontend/finance/src/screens/login/login.tsx
@@ -11,12 +11,17 @@ const Login = () => {
             <Header />
             <MainContainer 
             mainClass="loginmaincontainer"
+            containerClass="cstm_flex flex_j-center "
             children = {
 
                 <>
 
-                    <div className="loginparent cstm_flex flex_j-center  gap_cstm">
-                        <h1>asdsad</h1>
+                <div className="loginparent cstm_flex  flex_d-col gap_cstm w-500px">
+                        <h1 className="h1-big">Login</h1>
+                        <form action={"#"} className="cstm_flex gap_cstm flex_d-col">
+                            <label className="p-normal">Email id or Username<span className="color-danger"> *</span></label>
+                            <input type="text" className="inputcstm" />
+                        </form>
                     </div>
                 </>
             }
